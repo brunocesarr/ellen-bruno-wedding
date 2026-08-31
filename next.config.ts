294e73bd9e9d72c959e8b45d58e07dcd6a61571f
bruno.cesar <bruno.cesar@dtidigital.com.br>
2026-08-30 23:44:10 -0300

    feat: Add allowlist.

--- next.config.ts
+++ next.config.ts
@@ -53,7 +53,7 @@ const nextConfig: NextConfig = {
           },
           {
             key: 'Permissions-Policy',
-            value: 'camera=(), microphone=(), geolocation=(), payment=()',
+            value: 'camera=(), microphone=(), geolocation=(), payment=(self "https://*.mercadopago.com" "https://*.mercadolibre.com")',
           },
           { key: 'X-DNS-Prefetch-Control', value: 'on' },
         ],
